39d113b5c7632f137fe3f9d65abe2f388f73d6aa
Somkelechi Vera <somkelechivera@gmail.com>
2025-08-21 23:37:22 +0100

    Update SQL script

--- first-project.sql
+++ first-project.sql
@@ -1,3 +1,7 @@
+-- This is a beginner friendly database to help anyone starting out their journey in MYSQL.
+
+-- Follow the instructions below to create a new data base from scratch using MYSQL 
+
 /* Create a new table called persons
 with columns: id, person_name, birth_date and phone */ 
 
@@ -9,6 +13,13 @@ CREATE TABLE person (
     CONSTRAINT pk_person PRIMARY KEY (id)
 );
 
+-- To edit your table 
+
 -- Add a new column called email to the persons table
+
 ALTER TABLE persons
 ADD email VARCHAR (50) NOT NULL;
+
+-- Remove the column phone from the persons table
+ALTER TABLE persons
+DROP COLUMN phone;
