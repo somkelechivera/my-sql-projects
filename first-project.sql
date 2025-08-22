c0774922949828400dd62514c6a238962b807a6c
Somkelechi Vera <somkelechivera@gmail.com>
2025-08-22 11:32:03 +0100

    Update SQL script

--- first-project.sql
+++ first-project.sql
@@ -26,6 +26,15 @@ DROP COLUMN phone;
 
 -- To add information into the table, use the command below
 
+
 INSERT INTO persons (id, person_name, birth_date, email)
-VALUES (1, 'Anna', NULL, 'anna@gmail.com'),
-       (2, 'Sam', NULL, 'sam@gmail.com');
+VALUE (1, 'Maria', NULL, 'Unknown'),
+      (2, 'John', NULL, 'Unknown'),
+      (3, 'George', NULL, 'Unknown'),
+      (4, 'Martin', NULL, 'Unknown'),
+      (5, 'Peter', NULL, 'Unknown'),
+      (6, 'Anna', NULL, 'Unknown'),
+      (7, 'Sam', NULL, 'Unknown'),
+      (8, 'Vera', NULL, 'Unknown'),
+      (9, 'Andreas', NULL, 'Unknown'),
+      (10, 'Sarah', NULL, 'Unknown');
